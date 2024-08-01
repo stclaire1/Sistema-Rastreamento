fab25e5e7e48ee95d7aa2a43439ee91d4ded7799
aluno <aluno@udicentro.local>
2024-08-01 19:38:51 -0300

    criando projeto

---
+++ src/main/java/iftm/edu/br/rastreio/RastreioApplication.java
@@ -0,0 +1,13 @@
+package iftm.edu.br.rastreio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RastreioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RastreioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iftm/edu/br/rastreio/RastreioApplicationTests.java
@@ -0,0 +1,13 @@
+package iftm.edu.br.rastreio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RastreioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
